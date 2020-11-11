13db096bb42e4fda61e39cd8405d56c58641062e
CBrillant <2423459133@qq.com>
2020-11-12 00:52:22 +0800

    add support for regular expression's static check

--- src/main/java/dev/compiler/ReConvert.java
+++ src/main/java/dev/compiler/ReConvert.java
@@ -13,7 +13,68 @@ public class ReConvert {
             if (reExpr.charAt(i) != ' ') str.append(reExpr.charAt(i));
         }
         reExpr = str.toString();
-        state = 0;
+        try {
+            if (checkValidity(reExpr)) {
+                state = 0;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            // 咋处理？
+        }
+    }
+
+    /**
+     * 检查条件：
+     * 1.不允许出现英文字母、*、|、(、) 之外的其他字符
+     * 2.()括号要匹配
+     * 3.不允许 ||  |*  ** 这四种情况出现
+     *
+     * @param str 待检查的正规式
+     */
+    private boolean checkValidity(String str) throws Exception {
+
+        //检查是否有非要求字符输入
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (!isValidateChar(c)) {
+                throw new Exception("输入非法字符");
+            }
+        }
+
+        //检查括号匹配、是否有|| ** |* 出现
+        int count = 0;//记录（ 、）数量
+        for (int i = 0; i < str.length() - 1; i++) {
+            char c1 = str.charAt(i);
+            char c2 = str.charAt(i + 1);
+            if (c1 == '(') {
+                count++;
+            } else if (c1 == ')') {
+                count--;
+            }
+            if (c1 == '|' && (c2 == '*' || c2 == '|')) {
+                throw new Exception("|后不能有|或*");
+            } else if (c1 == '*' && c2 == '*') {
+                throw new Exception("*后能能有*");
+            }
+        }
+        if (str.charAt(str.length() - 1) == ')') {
+            count--;
+        }
+        if (count != 0) {
+            throw new Exception("括号不匹配");
+        } else {
+            return true;
+        }
+    }
+
+    private boolean isValidateChar(char c) {
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+            return true;
+        } else if (c == '|' || c == '*' || c == '(' || c == ')') {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -114,28 +175,28 @@ public class ReConvert {
      * @param right 自动机2
      */
 
-    private NFA makeNFA_OR(NFA left, NFA right){
+    private NFA makeNFA_OR(NFA left, NFA right) {
         int flag = 0;
         for (Edge edge : right.getStart().getNxt()) {
-            System.out.println("[edge]:"+edge);
+            System.out.println("[edge]:" + edge);
             for (char c : edge.getAllTransitions().toCharArray()) {
-                if(edge.to.equals(right.getEnd())){
-                    left.getStart().addNextNode(left.getEnd(),edge.id,c);  //right只有一条边相连
+                if (edge.to.equals(right.getEnd())) {
+                    left.getStart().addNextNode(left.getEnd(), edge.id, c);  //right只有一条边相连
                     flag = 1;
-                }else{
-                    left.getStart().addNextNode(edge.to,edge.id,c);  //right有多条边相连
+                } else {
+                    left.getStart().addNextNode(edge.to, edge.id, c);  //right有多条边相连
                 }
             }
         }
 
         for (Edge edge : right.getEnd().getPrev()) {
-            if(flag==1)
+            if (flag == 1)
                 break;
             for (char c : edge.getAllTransitions().toCharArray()) {
-                if(edge.to.equals(right.getStart()))
-                    left.getStart().addNextNode(left.getEnd(),edge.id,c);
-                else{
-                    edge.to.addNextNode(left.getEnd(),edge.id,c);
+                if (edge.to.equals(right.getStart()))
+                    left.getStart().addNextNode(left.getEnd(), edge.id, c);
+                else {
+                    edge.to.addNextNode(left.getEnd(), edge.id, c);
                     for (Edge edge1 : edge.to.getNxt()) {
                         edge.to.reNxtEdge(edge1);
                     }
@@ -143,7 +204,7 @@ public class ReConvert {
             }
         }
 
-        return new NFA(left.getStart(),left.getEnd());
+        return new NFA(left.getStart(), left.getEnd());
     }
 
     /**
@@ -151,11 +212,11 @@ public class ReConvert {
      *
      * @param op 自动机
      */
-    private NFA makeNFA_CL(NFA op){
+    private NFA makeNFA_CL(NFA op) {
         Node node = new Node(String.valueOf(++state));//新建一个点，用做闭包的基点
-        node.addNextNode(op.getStart(),'Ɛ');  //基点空转移到自动机op的初始节点
-        op.getEnd().addNextNode(node,'Ɛ');  //自动机op的终止节点空转移到基点
-        return new NFA(node,node);
+        node.addNextNode(op.getStart(), 'Ɛ');  //基点空转移到自动机op的初始节点
+        op.getEnd().addNextNode(node, 'Ɛ');  //自动机op的终止节点空转移到基点
+        return new NFA(node, node);
     }
 
     /**
@@ -165,12 +226,12 @@ public class ReConvert {
      * @param right 自动机2
      */
 
-    private NFA makeNFA_AND(NFA left, NFA right){
+    private NFA makeNFA_AND(NFA left, NFA right) {
         for (Edge edge : right.getStart().getNxt()) {
             for (char c : edge.getAllTransitions().toCharArray()) {
-                left.getEnd().addNextNode(edge.to,edge.id,c);
+                left.getEnd().addNextNode(edge.to, edge.id, c);
             }
         }
-        return new NFA(left.getStart(),right.getEnd());
+        return new NFA(left.getStart(), right.getEnd());
     }
 }
